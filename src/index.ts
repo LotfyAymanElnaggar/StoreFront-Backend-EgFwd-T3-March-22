b3155e8665e06611d0bf472634a5da31a7b817ab
LotfyAymanElnaggar <lotfyaymanelnaggar@gmail.com>
2022-06-20 08:14:33 +0200

    init index

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from 'express'
+import morgan from 'morgan'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+const app: Application = express()
+
+app.use(morgan('short'))
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello in our 🌍'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is starting at prot:${PORT}`)
+})
+
+export default app
