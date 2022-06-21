f5a4578f12601aaae9754f881263c720f62779e8
LotfyAymanElnaggar <lotfyaymanelnaggar@gmail.com>
2022-06-21 13:39:45 +0200

    Add uuid

--- migrations/sqls/20220621040711-users-up.sql
+++ migrations/sqls/20220621040711-users-up.sql
@@ -1,5 +1,6 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTs users (
-    id SERIAL PRIMARY Key,
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     fullname VARCHAR(50) NOT NULL
