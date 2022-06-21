3cb83d03e750f6579e85d4c2fb976b29a0bf4852
LotfyAymanElnaggar <lotfyaymanelnaggar@gmail.com>
2022-06-21 19:54:24 +0200

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import usersRoutes from './users.routes'
+import productsRoutes from './products.routes'
+import ordersRoutes from './orders.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
+routes.use('/products', productsRoutes)
+routes.use('/orders', ordersRoutes)
 
 export default routes
